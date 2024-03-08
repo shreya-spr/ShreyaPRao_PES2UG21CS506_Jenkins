31ea4b1c9134ec81f1db337a48b42ed0a6bfafb9
shreya-spr <92443502+shreya-spr@users.noreply.github.com>
2024-03-08 16:33:01 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+//File: main.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, Jenkins\n";
+  cout << "Hello, I created the main file cpp\n";
+  cout << "I have successfully built and run\n";
+  return 0;
+}
